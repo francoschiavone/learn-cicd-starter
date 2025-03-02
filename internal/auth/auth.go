0a1ad358490dd02b462fd9844349f1011203d896
francoschiavone <francoaschiavone@gmail.com>
2025-03-01 22:18:27 -0300

    Modify GetAPIKey Function to test CI fail

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -9,15 +9,20 @@ import (
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
-func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
-	}
-	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
-	}
+// func GetAPIKey(headers http.Header) (string, error) {
+// 	authHeader := headers.Get("Authorization")
+// 	if authHeader == "" {
+// 		return "", ErrNoAuthHeaderIncluded
+// 	}
+// 	splitAuth := strings.Split(authHeader, " ")
+// 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+// 		return "", errors.New("malformed authorization header")
+// 	}
 
-	return splitAuth[1], nil
+// 	return splitAuth[1], nil
+// }
+
+// GetAPIKey - intentionally broken version for CI testing
+func GetAPIKey(headers http.Header) (string, error) {
+	return "", errors.New("intentional break: always fail")
 }
